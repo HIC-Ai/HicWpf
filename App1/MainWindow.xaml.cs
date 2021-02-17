effd86c66aa00f311684340f8ffb1324ac15d294
abdulrahmman Mostafa <abdulRahman.Mostafa30@hotmail.com>
2021-02-17 09:22:27 +0200

    Schedule add

--- App1/MainWindow.xaml.cs
+++ App1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Syncfusion.UI.Xaml.Scheduler;
 
 namespace App1
 {
@@ -20,6 +21,7 @@ namespace App1
     /// </summary>
     public partial class MainWindow : Window
     {
+        SfScheduler schedule = new SfScheduler();
         public MainWindow()
         {
             InitializeComponent();
